89009992e87f5d44e208f8e40bb1fbcdfbbe8e8b
Franco Salas <franco_10_65@hotmail.com>
2024-11-27 15:04:44 -0300

    Agregando contenido a nueva rama

--- src/Pelicula.java
+++ src/Pelicula.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
-
 public class Pelicula {
+    String nombre;
+    int anioDeLanzamiento;
+    int duracionEnMinutos;
+    boolean incluidoEnElPlan;
+    double sumaDeLasEvaluaciones;
+    int totalDeEvaluaciones;
+
+    //metodos
+    void muestraFichaTecnica(){
+        System.out.println("Pelicula: " + nombre);
+        System.out.println("Año de lanzamiento: " + anioDeLanzamiento);
+        System.out.println("Duracion : " + duracionEnMinutos);
+
+    }
+
+    void evalua(double nota){
+        sumaDeLasEvaluaciones += nota;
+        totalDeEvaluaciones ++;
+    }
+
+    double calculaMedia(){
+        return sumaDeLasEvaluaciones/totalDeEvaluaciones;
+    }
+
 }
--- src/Principal.java
+++ src/Principal.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
-
 public class Principal {
+    public static void main(String[] args) {
+        Pelicula miPelicula = new Pelicula();
+
+        miPelicula.nombre = "Matrix";
+        miPelicula.anioDeLanzamiento = 1999;
+        miPelicula.duracionEnMinutos = 120;
+        miPelicula.incluidoEnElPlan = true;
+
+        miPelicula.muestraFichaTecnica();
+        miPelicula.evalua(10);
+        miPelicula.evalua(8);
+
+        System.out.println("Evaluaciones: " + miPelicula.sumaDeLasEvaluaciones);
+        System.out.println("media: " +miPelicula.calculaMedia());
+
+        Pelicula otraPelicula = new Pelicula();
+
+        otraPelicula.nombre = "Dragon ball Z";
+        otraPelicula.anioDeLanzamiento = 1990;
+        otraPelicula.duracionEnMinutos = 120;
+        otraPelicula.incluidoEnElPlan = true;
+
+     //   otraPelicula.muestraFichaTecnica();
+    }
 }
