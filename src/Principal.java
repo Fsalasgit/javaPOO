0939de71c974e5af0e9890426cf0b54c725be19f
Franco Salas <franco_10_65@hotmail.com>
2024-11-27 15:51:43 -0300

    getter y setter

--- src/Principal.java
+++ src/Principal.java
@@ -1,25 +1,27 @@
+import com.franco.screenmatch.modelos.Pelicula;
+
 public class Principal {
     public static void main(String[] args) {
         Pelicula miPelicula = new Pelicula();
 
-        miPelicula.nombre = "Matrix";
-        miPelicula.anioDeLanzamiento = 1999;
-        miPelicula.duracionEnMinutos = 120;
-        miPelicula.incluidoEnElPlan = true;
+        miPelicula.setNombre("Matrix");
+        miPelicula.setAnioDeLanzamiento(1999);
+        miPelicula.setDuracionEnMinutos(120);
+        miPelicula.setIncluidoEnElPlan(true);
+
 
         miPelicula.muestraFichaTecnica();
         miPelicula.evalua(10);
         miPelicula.evalua(8);
 
-        System.out.println("Evaluaciones: " + miPelicula.sumaDeLasEvaluaciones);
+
+        System.out.println("El nombre desde el get es: "+ miPelicula.getNombre());
+
+
+        System.out.println("Total de evaluaciones: " + miPelicula.getTotalDeEvaluaciones());
+
+
         System.out.println("media: " +miPelicula.calculaMedia());
-
-        Pelicula otraPelicula = new Pelicula();
-
-        otraPelicula.nombre = "Dragon ball Z";
-        otraPelicula.anioDeLanzamiento = 1990;
-        otraPelicula.duracionEnMinutos = 120;
-        otraPelicula.incluidoEnElPlan = true;
 
      //   otraPelicula.muestraFichaTecnica();
     }
